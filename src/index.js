c1a2028c86e7d03023b317dc2b1cf8081e76942c
shruti_kadam <shruti_kadam@magicsoftware.com>
2024-09-18 12:42:20 +0530

    changes in index.js

--- src/index.js
+++ src/index.js
@@ -5,11 +5,11 @@ import App from "./App";
 import { DataLayer } from "./DataLayer";
 import reducer, { initialState } from "./reducer";
 
-ReactDOM.render(
-    <React.StrictMode>
-        <DataLayer initialState={initialState} reducer={reducer}>
-            <App />
-        </DataLayer>
-    </React.StrictMode>,
-    document.getElementById("root")
+root.render(
+  <React.StrictMode>
+    <DataLayer initialState={initialState} reducer={reducer}>
+      <App />
+    </DataLayer>
+  </React.StrictMode>,
+  document.getElementById("root")
 );
